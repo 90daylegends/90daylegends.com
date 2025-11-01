913d6d641f282ae1687f1f2590b82a3a02aaed47
90daylegends <90daylegends@gmail.com>
2025-11-01 21:52:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const playButton = document.getElementById("playButton");
-const teaser = document.getElementById("teaser");
-
-playButton.addEventListener("click", () => {
-  teaser.play();
-  teaser.muted = false;
-  playButton.style.display = "none";
-});
